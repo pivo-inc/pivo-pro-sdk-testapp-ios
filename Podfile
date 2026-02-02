75a349bf7c41baf17f770241a736af98e8ca4ac9
tass-kwon <tass.kwon@3i.ai>
2026-02-02 13:57:25 +0900

    feat: Easing

--- Podfile
+++ Podfile
@@ -6,7 +6,6 @@ target 'PivoProSDKTestApp' do
   use_frameworks!
 
   # Pods for PivoProSDKTestApp
-  # pod 'PivoProSDK', :git => 'https://github.com/pivo-inc/pivo-pro-sdk-ios.git', :tag => '1.0.2'
-#  pod 'PivoProSDK', :path => '../../SDK/mobile.ios.sdk.pro'
+   pod 'PivoProSDK', :git => 'https://github.com/pivo-inc/pivo-pro-sdk-ios.git', :tag => '1.0.3'
 
 end
--- Pods/Target Support Files/Pods-PivoProSDKTestApp/Pods-PivoProSDKTestApp-frameworks.sh
+++ Pods/Target Support Files/Pods-PivoProSDKTestApp/Pods-PivoProSDKTestApp-frameworks.sh
@@ -176,10 +176,10 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/PivoProSDK/PivoProSDK.framework"
+  install_framework "${PODS_ROOT}/PivoProSDK/PivoProSDK.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/PivoProSDK/PivoProSDK.framework"
+  install_framework "${PODS_ROOT}/PivoProSDK/PivoProSDK.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
